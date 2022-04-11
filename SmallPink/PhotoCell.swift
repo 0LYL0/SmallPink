c44fc39deafca233867c6881b84f0098dec98386
yalan <1171368443@qq.com>
2022-04-11 18:35:14 +0800

    发布选择图片展示

---
+++ SmallPink/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  SmallPink
+//
+//  Created by yalan on 2022/4/11.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ SmallPink/PhotoFooterView.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoFooterView.swift
+//  SmallPink
+//
+//  Created by yalan on 2022/4/11.
+//
+
+import UIKit
+
+class PhotoFooterView: UICollectionReusableView {
+        
+}
