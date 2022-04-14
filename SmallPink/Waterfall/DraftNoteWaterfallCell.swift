d09704ca707903ab85fbc374e9854bfeb2422517
yalan <1171368443@qq.com>
2022-04-14 19:03:34 +0800

    存笔记草稿+草稿页面

---
+++ SmallPink/Waterfall/DraftNoteWaterfallCell.swift
@@ -0,0 +1,12 @@
+//
+//  DraftNoteWaterfallCell.swift
+//  SmallPink
+//
+//  Created by yalan on 2022/4/14.
+//
+
+import UIKit
+
+class DraftNoteWaterfallCell: UICollectionViewCell {
+    
+}
---
+++ SmallPink/Waterfall/WaterfallVC-loadData.swift
@@ -0,0 +1,8 @@
+//
+//  WaterfallVC-loadData.swift
+//  SmallPink
+//
+//  Created by yalan on 2022/4/14.
+//
+
+import Foundation
